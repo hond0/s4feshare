1dc502c9f4229a103798f6f33b99779f394411bb
mzell <marius.zell@n4group.de>
2016-07-19 15:15:47 +0200

    some startup stuff

---
+++ src/Main/java/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author marius_work
+ *
+ */
+package Main.java;
---
+++ src/Main/java/startUp.java
@@ -0,0 +1,36 @@
+package Main.java;
+
+import java.applet.Applet;
+
+public class startUp extends Applet {
+	 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+ 
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		super.start();
+	}
+ 
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+ 
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+ 
+}
